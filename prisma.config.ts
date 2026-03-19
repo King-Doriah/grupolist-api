9fd5d5e38357c1ec50a3169001a678f0b597ecf4
King-Doriah <miraldinodoria302@gmail.com>
2026-03-19 07:07:42 +0100

    fix: resolve merge conflict in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,29 +2,6 @@ import "dotenv/config";
 import type { PrismaConfig } from "prisma";
 import { env } from "prisma/config";
 
-<<<<<<< HEAD
-=======
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-export const adapter = (type: "mysql" | "postgresql") => {
-  if (type === "postgresql")
-    return new PrismaPg({
-      connectionString: env("DATABASE_URL"),
-    });
-  else if (type === "mysql")
-    return new PrismaMariaDb({
-      host: env("DATABASE_HOST"),
-      user: env("DATABASE_USER"),
-      password: env("DATABASE_PASSWORD"),
-      database: env("DATABASE_NAME"),
-      connectionLimit: 5,
-    });
-  else return;
-};
-
->>>>>>> cb04f4daefe8e8899b99c99450f6a540ea3b1297
 export default {
   schema: "prisma/schema.prisma",
   migrations: {
