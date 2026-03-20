8550fd1a909378ebc88a1daa5ee3b540bacf856f
King-Doriah <miraldinodoria302@gmail.com>
2026-03-20 01:51:18 +0100

    fix: prisma config paths

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,9 @@ import type { PrismaConfig } from "prisma";
 import { env } from "prisma/config";
 
 export default {
-  schema: "prisma/schema.prisma",
+  schema: "./prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "./prisma/migrations",
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
